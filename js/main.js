e013114bcefa915fd71b67baa387921993e2c2c9
Beomene <trinkaloop@gmail.com>
2025-05-08 02:16:57 +0200

    Add files via upload
    Signed-off-by: Beomene <trinkaloop@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,87 +1,112 @@
 class TrinkareadEngine {
-    constructor() {
-        this.scenes = [];
-        this.currentScene = 0;
-        this.sceneHeight = 1600;
-        this.init();
-    }
+  constructor() {
+    this.scenes = [];
+    this.currentScene = 0;
+    this.sceneHeight = 1600;
+    this.init();
+  }
 
-    async init() {
-        await this.loadSceneSequence('01_01');
-        this.renderScene(0);
-        this.setupInertialScroll();
-    }
+  async init() {
+    await this.loadSceneSequence('01_01');
+    this.renderScene(0);
+    this.setupInertialScroll();
+    this.hideLoader();
+  }
 
-    async loadSceneSequence(chapterPage) {
-        this.scenes = [
-            `${chapterPage}_001`,
-            `${chapterPage}_002`,
-            `${chapterPage}_003`,
-            `${chapterPage}_004`,
-            `${chapterPage}_005`
-        ];
-    }
+  async loadSceneSequence(chapterPage) {
+    this.scenes = [
+      `${chapterPage}_001`,
+      `${chapterPage}_002`,
+      `${chapterPage}_003`,
+      `${chapterPage}_004`,
+      `${chapterPage}_005`
+    ];
+  }
 
-    async renderScene(index) {
-        const sceneId = this.scenes[index];
-        const config = await this.loadConfig(sceneId);
-        const text = await this.loadText(config.text);
-        
-        document.getElementById('page-container').innerHTML = `
-            <div class="block" data-scene="${sceneId}">
-                ${this.generateLayers(config)}
-                <div class="text-cutout">
-                    ${text}
-                </div>
-            </div>
+  async renderScene(index) {
+    const sceneId = this.scenes[index];
+    const config = await this.loadConfig(sceneId);
+    const text = await this.loadText(config.text);
+    
+    document.getElementById('page-container').innerHTML = `
+      <div class="block" data-scene="${sceneId}">
+        ${this.generateLayers(config)}
+        <div class="text-cutout">
+          ${text}
+        </div>
+      </div>
+    `;
+    
+    this.currentScene = index;
+    window.scrollTo(0, this.sceneHeight * index);
+  }
+
+  async loadConfig(sceneId) {
+    const res = await fetch(`assets/blocks/${sceneId}/config.json`);
+    return await res.json();
+  }
+
+  async loadText(textPath) {
+    const res = await fetch(textPath);
+    return await res.text();
+  }
+
+  generateLayers(config) {
+    let layers = '';
+    
+    // Standard layers
+    ['sky', 'hyperdistal', 'distal', 'close', 'closer'].forEach(layer => {
+      if (config.layers[layer]) {
+        layers += `
+          <div class="${layer}-layer parallax-layer">
+            <img src="${config.layers[layer]}" alt="${layer} layer">
+          </div>
         `;
-        
-        this.currentScene = index;
-        window.scrollTo(0, this.sceneHeight * index);
+      }
+    });
+
+    // Stickers
+    if (config.stickers) {
+      config.stickers.forEach(sticker => {
+        layers += `
+          <img class="sticker" src="${sticker.image}" 
+               style="${sticker.side || 'left'}: 50px; 
+                      top: ${sticker.y_offset || '30%'};">
+        `;
+      });
     }
 
-    async loadConfig(sceneId) {
-        const res = await fetch(`assets/blocks/${sceneId}/config.json`);
-        return await res.json();
-    }
+    return layers;
+  }
 
-    async loadText(textPath) {
-        const res = await fetch(textPath);
-        return await res.text();
-    }
+  setupInertialScroll() {
+    let velocity = 0;
+    let damping = 0.93;
+    let isScrolling = false;
 
-    generateLayers(config) {
-        let layers = '';
-        
-        // Standard layers
-        ['sky', 'hyperdistal', 'distal', 'close', 'closer'].forEach(layer => {
-            if (config.layers[layer]) {
-                layers += `
-                    <div class="${layer}-layer parallax-layer">
-                        <img src="${config.layers[layer]}" alt="${layer} layer">
-                    </div>
-                `;
-            }
-        });
+    window.addEventListener('wheel', (e) => {
+      e.preventDefault();
+      velocity += e.deltaY * 0.2;
+      if (!isScrolling) requestAnimationFrame(scrollTick);
+    }, { passive: false });
 
-        // Stickers
-        if (config.stickers) {
-            config.stickers.forEach(sticker => {
-                layers += `
-                    <img class="sticker" src="${sticker.image}" 
-                         style="${sticker.side || 'left'}: 50px; 
-                                top: ${sticker.y_offset || '30%'};">
-                `;
-            });
-        }
+    const scrollTick = () => {
+      isScrolling = true;
+      velocity *= damping;
+      window.scrollBy(0, velocity);
+      
+      if (Math.abs(velocity) > 0.5) {
+        requestAnimationFrame(scrollTick);
+      } else {
+        isScrolling = false;
+      }
+    };
+  }
 
-        return layers;
-    }
-
-    setupInertialScroll() {
-        // Previous inertial scroll implementation here
-        // (From our earlier discussion)
-    }
+  hideLoader() {
+    document.querySelector('.loader').style.display = 'none';
+  }
 }
 
+// Initialize
 const engine = new TrinkareadEngine();
